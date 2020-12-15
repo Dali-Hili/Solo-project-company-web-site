9a4bb8f81f9ff589604b0994be0b45e3c0cfa986
Dali-Hili <you@example.com>
2020-12-15 15:40:55 +0100

    project

---
+++ main.js
@@ -0,0 +1,19 @@
+$('#first').slideToggle('slow')
+$('#first').slideDown('slow')
+$('.exam').slideToggle('slow')
+$('.exam').slideDown('slow')
+
+
+
+$("a").first().click(function(){ 
+ $(".sec").css("background-color","#778899");
+ });
+$('#log').css('display', 'inline');
+
+
+
+$(document).ready(function(){
+    $(this).scrollTop(0,0);
+});
+
+
